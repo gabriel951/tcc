1e586b128dc4f581bcdc58983fdd5343427bb937
gabriel951 <gabrieltiburciouaiso@gmail.com>
2017-01-08 08:39:03 -0200

    in the middle of third phase of crisp-dm

---
+++ code/students/students.py
@@ -0,0 +1,33 @@
+# student file, contain information about the students
+
+class Student(): 
+    """
+    class that represents the student
+    """
+
+    def __init__(self, id_num): 
+        """
+        initializes a student, does not put any information
+        """
+        # id number
+        self.id_num = id_num
+
+        # registration
+        self.reg = None
+        
+        # social data follows
+        self.sex = None
+        self.age = None
+        self.quota = None
+        self.school_type = None
+        self.race = None
+        self.local = None
+        self.course = None
+        self.year_in = None
+        self.sem_in = None
+        self.year_out = None
+        self.sem_out = None
+        self.way_out = None
+
+        # performance data
+        self. 
